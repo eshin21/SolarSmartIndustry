926a563d09c153529f11b74e3d29a426b55b12f7
Enoch Shin <eshin135@gmail.com>
2026-02-24 17:58:28 +0100

    data pre-process

--- WeatherData/data_preprocessing.py
+++ WeatherData/data_preprocessing.py
@@ -3,7 +3,7 @@ import pandas as pd
 from pathlib import Path
 
 def main():
-    folder_path = Path(__file__).parent / "Data"
+    folder_path = str(Path(__file__).parent / "Data")
     file_name = "Dades_XEMA_20260217.csv"
 
     df = pd.DataFrame()
@@ -11,18 +11,20 @@ def main():
     # date_format = '%Y-%m-%d %H:%M:%S'  #EDITED: Specify the date format of the downloaded data.
 
 
-    with open(folder_path / file_name, 'rt', encoding='utf-8') as f:
+    with open(os.path.join(folder_path, file_name), 'rt', encoding='utf-8') as f:
 
         df = pd.read_csv(f, sep=',', encoding='utf-8', header=0, engine='pyarrow')  #EDITED: Complete with the remaining parts of the read_csv function.
 
     # TODO - Complete the remaining parts of the script.
-    df.DATA_LECTURA = pd.to_datetime(df['DATA_LECTURA'], format=date_format) #EDITED: Convert DATA_LECTURA to datetime
+    df.DATA_LECTURA = pd.to_datetime(df['DATA_LECTURA'], format='mixed', dayfirst=True) #EDITED: Convert DATA_LECTURA to datetime
     df.sort_values(by='DATA_LECTURA', inplace=True) #EDITED: Sort by date
     df.reset_index(inplace=True, drop=True)
 
     data_weather = df.pivot(index='DATA_LECTURA', columns='CODI_VARIABLE', values='VALOR_LECTURA') #EDITED: Pivot table to get variables as columns
     data_weather.insert(0, 'DateTime', data_weather.index)
     data_weather.insert(1, 'CodiEstacio', df.CODI_ESTACIO[0])
+
+
     data_weather.rename(columns={
         30: 'Wind', #EDITED: Map code 30
         31: 'WindDirection', #EDITED: Map code 31
@@ -38,11 +40,8 @@ def main():
         44: 'RelativeHumidityMin', #EDITED: Map code 44
         3:  'RelativeHumidityMax', #EDITED: Map code 46
 
-        56: 'WindGust_RatxaMaximaVent2m', #EDITED: Windgust Just in Caseç
-        57: 'WindDirectionGust_2m', #EDITED: Map code 51
-
-        53: 'WindGust_RatxaMaximaVent6m', #EDITED: Windgust Just in Case
-        54: 'WindDirectionGust_6m', #EDITED: Map code 51
+        50: 'WindGust',
+        51: 'WindDirectionGust',
         72: 'RainMax', #Precipitacio maxima en 1 minut.
 
         }, inplace=True) #EDITED: Check information about the variable names in the useful information part.
